dbfcfdf5bb8692d08ee4d284dc7fab9d7077ac4b
Stephen <ck19955@bristol.ac.uk>
2022-08-10 10:43:28 +0100

    Introduced a function that plays a round of whist where each player plays a card (currently at random). I have also included previously made functions (get_suit() and compare_cards()) which allow me to find out which player won the round and therefore, which player starts the next round first.

--- WhistGameSetup.py
+++ WhistGameSetup.py
@@ -38,14 +38,54 @@ def create_players(card_deck, num_players, number_cards):
 def bidding(player_hand, num_players_after, sum_bids):
     bid = random.randint(0, len(player_hand))
     if num_players_after == 0:
-        print("wow")
         while bid + sum_bids == len(player_hand):
             bid = random.randint(0, len(player_hand))
     return bid
 
 
+def get_suit(card_number):
+    if card_number < 13:
+        return 0
+    elif 13 <= card_number < 26:
+        return 1
+    elif 26 <= card_number < 39:
+        return 2
+    else:
+        return 3
+
+
+def compare_cards(card1, card2, trump_suit):
+    if get_suit(card1) == get_suit(card2) and card2 > card1:
+        return True
+    elif get_suit(card2) == trump_suit and get_suit(card1) != trump_suit:
+        return True
+    else:
+        return False
+
+
+def play_round(game_info, start_player):
+    num_players = len(game_info)
+    table_cards = [0]*num_players
+    winning_pos = 0
+    winning_player = 0
+
+    for player in range(num_players):
+        current_player = (player + start_player) % num_players
+        current_hand = game_info[current_player].card
+        played_card = random.choice(current_hand)
+        game_info[current_player].card.remove(played_card)
+        table_cards[current_player] = played_card
+
+    for player in range(1, num_players):
+        current_player = (player + start_player - 1) % num_players
+        if compare_cards(table_cards[winning_pos], table_cards[player], 5):
+            winning_player = current_player
+            winning_pos = player
+    return table_cards, winning_player
+
+
 deck_of_cards = create_deck()
 player_info = create_players(deck_of_cards, 4, 13)
-
+print(play_round(player_info, 1))
 
 
